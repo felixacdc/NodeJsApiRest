bb2e7d09293913e6b3dbb2ab99f70202f07c6dae
felixacdc <pfmata01@gmail.com>
2016-10-10 22:33:55 -0600

    [add] Node.js API REST Tutorial #03 - Instalando body-parser y nodemon :metal:

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.listen(3000, () => {
-    console.log('API REST corriendo en http://localhost:3000');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`API REST corriendo en http://localhost:${port}`);
 });
