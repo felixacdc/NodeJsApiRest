1fb1b88d0d767c7edf3c968c563d511776926752
felixacdc <pfmata01@gmail.com>
2016-10-23 15:24:11 -0600

    [add] Node.js API REST Tutorial #11 - Refactorizando el código de nuestro proyecto

---
+++ controllers/product.js
@@ -0,0 +1,22 @@
+function getProduct (id) {
+    
+}
+
+function getProducts () {
+    
+}
+
+function updateProduct (id) {
+    
+}
+
+function deleteProduct (id) {
+    
+}
+
+exports = {
+    getProduct,
+    getProducts,
+    updateProduct,
+    deleteProduct
+}
