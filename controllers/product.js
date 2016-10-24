fe4623e72f8699562762c00bb4a2f47b033f867c
felixacdc <pfmata01@gmail.com>
2016-10-23 20:02:34 -0600

    [modify] Node.js API REST Tutorial #11 - Refactorizando el código de nuestro proyecto

--- controllers/product.js
+++ controllers/product.js
@@ -1,22 +1,79 @@
-function getProduct (id) {
+'use strict';
+
+const Product = require('../models/product');
+
+function getProduct (req, res) {
+    let productId = req.params.productId;
     
+    Product.findById(productId).then((product) => {
+        
+        if(!product) 
+            res.status(404).send({message: `El producto no existe`});
+        res.status(200).send({ product });
+        
+    }).catch((err) => {
+        res.status(500).send({message: `Error al guardar los datos: ${err}`});
+    });
 }
 
-function getProducts () {
-    
+function getProducts (req, res) {
+     Product.find({}).then((products) => {
+        if(!products) 
+            res.status(404).send({message: `El producto no existe`});
+        
+        res.status(200).send({ products });
+    }).catch((err) => {
+        res.status(500).send({message: `Error al guardar los datos: ${err}`});
+    });
 }
 
-function updateProduct (id) {
+function saveProduct(req, res) {
+    console.log('POST /api/product');
+    console.log(req.body);
     
+    let product = new Product();
+    product.name = req.body.name;
+    product.picture = req.body.picture;
+    product.price = req.body.price;
+    product.category = req.body.category;
+    product.description = req.body.description;
+    
+    product.save().then((product) => {
+        res.status(200).send({ product });
+    }).catch((err) => {
+        res.status(500).send({message: `Error al guardar los datos: ${err}`});
+    });
 }
 
-function deleteProduct (id) {
+function updateProduct (req, res) {
+    let productId = req.params.productId;
+    let update = req.body;
     
+    Product.findByIdAndUpdate(productId, update).then((product) => {
+        res.status(200).send({ message: "Producto actualizado." });
+    }).catch((err) => {
+        res.status(500).send({message: `Error al guardar los datos: ${err}`});
+    });
 }
 
-exports = {
+function deleteProduct (req, res) {
+    let productId = req.params.productId;
+    
+    Product.findById(productId).then((product) => {
+        product.remove().then(() => {
+            res.status(200).send({ message: "El producto ha sido eliminado." });
+        }).catch((err) => {
+            res.status(500).send({message: `Error al guardar los datos: ${err}`});
+        });
+    }).catch((err) => {
+        res.status(500).send({message: `Error al guardar los datos: ${err}`});
+    });
+}
+
+module.exports = {
     getProduct,
     getProducts,
+    saveProduct,
     updateProduct,
     deleteProduct
 }
--- index.js
+++ index.js
@@ -1,86 +1,12 @@
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Promise = require('bluebird');
+const mongoose = require('mongoose'),
+      Promise = require('bluebird'),
+      app = require('./app');
+
 Promise.promisifyAll(mongoose);
 
-const Product = require('./models/product');
-
-const app = express();
 const port = process.env.PORT || 5000;
-
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-
-app.get('/api/product', (req, res) => {
-    Product.find({}).then((products) => {
-        if(!products) 
-            res.status(404).send({message: `El producto no existe`});
-        
-        res.status(200).send({ products });
-    }).catch((err) => {
-        res.status(500).send({message: `Error al guardar los datos: ${err}`});
-    });
-});
-
-app.get('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId;
-    
-    Product.findById(productId).then((product) => {
-        
-        if(!product) 
-            res.status(404).send({message: `El producto no existe`});
-        res.status(200).send({ product });
-        
-    }).catch((err) => {
-        res.status(500).send({message: `Error al guardar los datos: ${err}`});
-    });
-});
-
-app.post('/api/product', (req, res) => {
-    console.log('POST /api/product');
-    console.log(req.body);
-    
-    let product = new Product();
-    product.name = req.body.name;
-    product.picture = req.body.picture;
-    product.price = req.body.price;
-    product.category = req.body.category;
-    product.description = req.body.description;
-    
-    product.save().then((product) => {
-        res.status(200).send({ product });
-    }).catch((err) => {
-        res.status(500).send({message: `Error al guardar los datos: ${err}`});
-    });
-});
-
-app.put('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId;
-    let update = req.body;
-    
-    Product.findByIdAndUpdate(productId, update).then((product) => {
-        res.status(200).send({ message: "Producto actualizado." });
-    }).catch((err) => {
-        res.status(500).send({message: `Error al guardar los datos: ${err}`});
-    });
-});
-
-app.delete('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId;
-    
-    Product.findById(productId).then((product) => {
-        product.remove().then(() => {
-            res.status(200).send({ message: "El producto ha sido eliminado." });
-        }).catch((err) => {
-            res.status(500).send({message: `Error al guardar los datos: ${err}`});
-        });
-    }).catch((err) => {
-        res.status(500).send({message: `Error al guardar los datos: ${err}`});
-    });
-});
 
 mongoose.connect('mongodb://localhost/shop', (err, res) => {
     if (err) 
