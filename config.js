46aefebe8bb18cb1c3c93eb22396a92737d5547d
felixacdc <pfmata01@gmail.com>
2016-10-23 21:02:06 -0600

    [modify] Node.js API REST Tutorial #11 (finished)

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT || 5000,
+    db: process.env.MONGODB || 'mongodb://localhost/shop'
+}
--- index.js
+++ index.js
@@ -2,18 +2,17 @@
 
 const mongoose = require('mongoose'),
       Promise = require('bluebird'),
-      app = require('./app');
+      app = require('./app'),
+      config = require('./config');
 
 Promise.promisifyAll(mongoose);
 
-const port = process.env.PORT || 5000;
-
-mongoose.connect('mongodb://localhost/shop', (err, res) => {
+mongoose.connect(config.db, (err, res) => {
     if (err) 
         return console.log(`Error al conectar la base de datos: ${err}`);
     
     console.log('Conexion a la base de datos establecida...');
-    app.listen(port, () => {
-        console.log(`API REST corriendo en http://localhost:${port}`);
+    app.listen(config.port, () => {
+        console.log(`API REST corriendo en http://localhost:${config.port}`);
     });
 });
